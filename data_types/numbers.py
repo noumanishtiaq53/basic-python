9c651b2e90723a61b94b075ca9fae5e3a67f35d1
unknown <unknown>
2024-12-28 16:28:35 +0500

    numbers of python

---
+++ data_types/numbers.py
@@ -0,0 +1,10 @@
+print(2+4 , 2-4 , 2/4, 2*4)   #integers
+print(type(2+3))  #type  <class 'int'>
+print(type(4/2)) # <class 'float'>
+print(type(0))  # <class 'int'>
+
+print(type(20+1.1)) # <class 'float'>
+
+print( 2 ** 3) #power
+print(5 // 3)  #division return rounded off value
+print ( 5 % 4)  #remainder of division
